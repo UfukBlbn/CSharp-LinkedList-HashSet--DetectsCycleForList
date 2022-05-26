a4b782c3509c51e2ca7356a6d65cdd3af44f8f07
Ufuk Balaban <71414017+UfukBlbn@users.noreply.github.com>
2022-05-26 21:48:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DetectsCycleApp
+{   
+    class CustomLinkedList
+    {
+        
+        Node head {get;set;}
+
+        public class Node {
+
+            public int data {get;set;}
+
+            public Node next {get;set;}
+
+            public Node (int d)
+            {
+                data = d;
+                next = null;
+            }
+
+        }
+     
+
+        public Boolean hasCycle()
+        {
+            HashSet<Node> nodes = new HashSet<Node>();
+
+            Node current = head;
+
+            while (current!=null)
+            {
+                if (nodes.Contains(current))
+                {
+                    return true;
+                }
+                else {
+                   nodes.Add(current);
+                }
+                current = current.next;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            CustomLinkedList noCycleLinkedList = new CustomLinkedList();
+            Node firstNode = new Node(3);
+            Node secondNode = new Node(4);
+            Node thirdNode = new Node(5);
+            Node fourthNode = new Node(6);
+
+            noCycleLinkedList.head=firstNode;
+            firstNode.next=secondNode;
+            secondNode.next=thirdNode;
+            thirdNode.next=fourthNode;
+
+            Console.WriteLine(noCycleLinkedList.hasCycle());
+
+             CustomLinkedList cycleLinkedList = new CustomLinkedList();
+             cycleLinkedList.head = firstNode;
+             thirdNode.next=secondNode;
+             Console.WriteLine(cycleLinkedList.hasCycle());
+
+        }
+    }
+}
